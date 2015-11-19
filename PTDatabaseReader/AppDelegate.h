4b7d9b812f2473e6e0f88925024f9557375c0d15
tttpeng <ipengtaoxx@gmail.com>
2015-11-19 18:07:50 +0800

    first commit

---
+++ PTDatabaseReader/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  PTDatabaseReader
+//
+//  Created by Peng Tao on 15/11/19.
+//  Copyright © 2015年 Peng Tao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ PTDatabaseReader/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  PTDatabaseReader
+//
+//  Created by Peng Tao on 15/11/19.
+//  Copyright © 2015年 Peng Tao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
